ce0605609d9aa4996f75883998d8541177ab3d56
shubham860 <shubham.chauhan860@gmail.com>
2019-10-03 00:05:25 +0530

    update

--- task-manager/src/components/Dashboard.js
+++ task-manager/src/components/Dashboard.js
@@ -3,11 +3,31 @@ import PropTypes from 'prop-types'
 import Manager from '../components/Manager'
 
 class Dashboard extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      title : '',
+      description : '',
+      dueDate : new Date(),
+      show : false
+    }
+   console.log(this.state.title)
+  }
+
+change = () => {
+  this.setState({
+    show:true
+  })
+}
   render () {
+    const {title,description,dueDate} = this.state;
     return(
       <div class='main'>
         <div>
-            <Manager/>
+            <button type='submit' onClick={this.change} >show</button>
+            {
+              this.state.show && <Manager title={title} description={description} dueDate={dueDate}/>
+            }
         </div>
       </div>
     )
